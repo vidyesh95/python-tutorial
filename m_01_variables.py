7a01383244132bd927e859ce9469f552f2b2540f
Vidyesh Churi <vidyesh95@gmail.com>
2024-01-15 22:47:39 +0530

    refactor literals

---
+++ m_01_variables.py
@@ -0,0 +1,20 @@
+# Python Variables
+"""
+Variables are containers for storing data values.
+"""
+# Creating Variables
+"""
+Python has no command for declaring a variable.
+A variable is created the moment you first assign a value to it.
+"""
+# Example
+x = 5
+y = "Vidyesh"
+print(x)
+print(y)
+
+# Variables do not need to be declared with any particular type and can even change type after they have been set.
+# Example
+x = 4  # x is of type int
+x = "Sally"  # x is now of type str
+print(x+" is 4 years old")
